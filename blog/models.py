ad285ebdd93cba578fc75126514311770a577627
Nikhil Badyal <nikhill773384@gmail.com>
2022-01-11 12:51:19 +0530

    Added DB in Blog Project

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    excerpt = models.CharField(max_length=255)
+    image_name = models.CharField(max_length=255)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.title} {self.author} {self.date}"
